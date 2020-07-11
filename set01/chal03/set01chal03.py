f17258a4c47eae51c6dbf2435ee093c86a6262bf
Cyber <cyberchaud@gmail.com>
2020-07-11 19:04:04 -0400

    Challenge 3 redone

--- set01/chal03/set01chal03.py
+++ set01/chal03/set01chal03.py
@@ -3,108 +3,88 @@
 # Challenge 03
 
 # Single-byte XOR cipher
-# Hex encoded string has been XORd against a single character.
-# Tip - Make a scoring system for English plaintext and find the highest score and choose the one with the best output.
 
-from bitstring import BitArray
+# Used the pwn tools library to simplify
 
-import logging
-import base64
-import binascii
-
-def stringtobin(s):
-    a = BitArray(hex=s)
-    return a.bin
-
-def xor2bins(key, str):
-    #bin1 = int(b1, 2)
-    #bin2 = int(b2, 2)
-    returnbit = ''
-    for bit in str:
-        returnbit += bit
-        #xor = bin1 ^ bin2
-    return returnbit
-    #return hex(xor)[2:]
-
-def hextoascii(s):
-    logging.debug("Inside hextoBase64")
-    logging.debug("Value of s is: {}".format(s))
-    return binascii.unhexlify(s)
+from pwn import *
+from collections import defaultdict
+import struct
+from decimal import *
+from urllib import request
 
 logging.basicConfig(filename='set01chal03.log', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 logging.debug('Start of challenge')
+string1 = '1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736'
+logging.debug('String 1 is: {}'.format(string1))
 
-ciphertext = '1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736'
+logging.debug('Unhexing the string')
+unhexStrings = bytes.fromhex(string1)
+logging.debug('Unhexed value is: {}'.format(unhexStrings))
 
-print(bytes.fromhex(ciphertext))
-bytesciphertext = bytes.fromhex(ciphertext)
-logging.debug('The encoded string is: {}'.format(bytesciphertext))
 
-binstring = stringtobin(ciphertext)
-
-logging.debug('The bin value of the ciphertext is: {}'.format(binstring))
-
-def xorres(key, string):
-    x, upper = 0, len(key) - 1
-    xresult = ''
-    for bit in string:
-        #print(str(bit), str(x), str(key), str(int(bit) ^ int(key[x])))
-        if x == upper:
-            x = 0
-        else:
+def alphacounter(s):
+    # Counter(char for char in s.upper() if char.isalpha())
+    x = 0
+    for byte in s:
+        if char.isalpha():
             x += 1
-        xresult += str(int(bit) ^ int(key[x]))
-        #logging.debug('The xresult is: {}'.format(xresult))
-    return xresult
+    return x
 
-logging.debug('The ciphertext is: {}'.format(ciphertext))
+listBytes = []
+for byte in range(254):
+    #logging.debug(bytes([byte]))
+    xorValue = xor(unhexStrings, bytes([byte]))
+    #logging.debug(xorValue)
+    listByte = [byte, bytes([byte]), xorValue]
+    listBytes.append(listByte)
 
-listResults = []
-for i in range(0, 255):
-        #print(i)
-        key = format(i, 'b')
-        listResults.append(xorres(key, binstring))
-        #logging.debug('The bin xor value is: {}'.format(result))
+#print(*listBytes, sep="\n")
+#logging.debug(*listBytes, sep="\n")
 
-def bintoByteArray(b):
-    rList = []
-    x, upper = 0, 7
-    bithelper = ''
-    for bit in b:
-        bithelper += str(int(bit))
-        if x == upper:
-            x = 0
-            rList.append(int(bithelper, 2))
-            bithelper = ''
-        else:
-            x += 1
+freqs = defaultdict(int)
+total = 0
+logging.debug("Counting each character in the text")
+with open("pg24397.txt") as f:
 
-    return rList
+    for line in f:
+        for char in line:
+            char = char.lower()
+            #if char.isalpha():
+            freqs[char] += 1
+            total += 1
+
+logging.debug(freqs)
+
+freqtable = {}
+
+logging.debug("Calculating frequencies")
+logging.debug("Making the frequency table")
+for key in freqs:
+    freqtable[key] = (freqs[key] / total)
+
+logging.debug(freqtable)
+
+print(listBytes[0])
+hightotal = 0
+highest = []
+
+for x in listBytes:
+    #print(x[2])
+    total = 0
+    print(total)
+    print(x)
+    for b in x[2]:
+        #print(b)
+        #print(freqtable.get(chr(b), 0))
+        total = freqtable.get(chr(b), 0) + total
+    if total > hightotal:
+        highest = []
+        newhigh = []
+        newhigh.append(x)
+        newhigh.append(total)
+        highest.append(newhigh)
+        hightotal = total
 
 
-#logging.debug(xor2bins(key[0], binstring))
 
-for item in listResults:
-    resBytes = bintoByteArray(item)
-    print(''.join([chr(byte) for byte in resBytes]))
-
-def stringtobin(s):
-    a = BitArray(hex=s)
-    logging.debug(a.bin)
-    return a.bin
-
-#print(stringtobin('b'))
-# val = stringtobin('256')
-# print(str(val))
-# testkey = '1001'
-# x = 0
-
-# for bit in str(val):
-
-#    print(str(bit), str(x), str(testkey), str(int(bit) ^ int(testkey[x])))
-#    if x == 3:
-#        x = 0
-#    else:
-#        x += 1
-
-
+print(highest)
